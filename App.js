2be34e6acbb85ddf956201d5644cd0a8a273d1ed
nitesh goyal <niteshgoyal@niteshs-MacBook-Air.local>
2024-02-10 00:45:38 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,21 @@
+//Nested structure
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        [ React.createElement("h1", {}, "I am an H1 tag!"),
+        React.createElement("h2", {}, "I am an H2 tag!") ]
+    )
+);
+
+
+const heading = React.createElement(
+    "h1", 
+    {id: "heading", xyz: "abc"}, 
+    "Hello World from React!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
