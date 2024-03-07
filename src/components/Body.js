eb46d60750716a233d15a8962ad834fa669bb6da
nitesh goyal <niteshgoyal@niteshs-MacBook-Air.local>
2024-03-07 23:54:38 +0530

    Episode - 05 (useState)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/restaurantMockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [ listOfRestaurants, setListOfRestaurants ] = useState(restaurantList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button 
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >Top Rated Restaurants</button>
+      </div>
       <div className="res-container">
-        {restaurantList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             restaurantData={restaurant}
